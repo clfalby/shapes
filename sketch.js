8751d8b76ba60fe21f9fb7c6fe65138b466f38a9
clfalby <47993181+clfalby@users.noreply.github.com>
2020-05-11 19:00:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 //The setup function only happens once
+var x=0
+y=0
+
 function setup() {
 	createCanvas(500, 500); //create a 500px X 500px canvas
 }
@@ -9,5 +12,7 @@ function draw() {
   //circle
   stroke(255,255,127) // an RGB color for the circle's border
   fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
-  ellipse(width/2,height/2,20,20); // center of canvas, 20px dia
+  ellipse(mouseX,mouseY,y,x); // center of canvas, 20px dia
+	y=y+1;
+	x=x+1;
 }
